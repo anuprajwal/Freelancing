58b3ad292d344f4702fc4267f522db1bf5edba7e
ANUPRAJWAL <anuprajwal89@gmail.com>
2025-07-14 19:08:10 +0530

    added the functionality of filtering doctors, adding address

--- src/controllers/slots/showSlots.js
+++ src/controllers/slots/showSlots.js
@@ -1,8 +1,8 @@
 const { doctorSlots } = require("../../../models");
 
 const showSlots = async (req, res)=>{
-    const {id} = req.user.payload
-    const allSlots = await doctorSlots.findAll({where:{doctor_id : id}})
+    const {doctor_id} = req.body
+    const allSlots = await doctorSlots.findAll({where:{doctor_id}})
 
     if (!allSlots){
         return res.status(404).json({error:"Cant find the slots for the doctor. check filling out the extra doctor information"})
