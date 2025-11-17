b09ecfccff999ab0d180cd608fffe4393f4be954
ANUPRAJWAL <anuprajwal89@gmail.com>
2025-11-17 21:59:35 +0530

    pushing

--- src/controllers/registration/forgotPassword.js
+++ src/controllers/registration/forgotPassword.js
@@ -3,6 +3,10 @@ const {User} = require("../../../models")
 const forgotPassword = async (req, res)=>{
     const {email, role} = req.body
 
+    if (!email || !role){
+        return res.status(400).json({error:"all fields are required"})
+    }
+
     const userData = await User.findOne({
         where:{
             email,
@@ -17,17 +21,17 @@ const forgotPassword = async (req, res)=>{
     const {id, password_hash} = userData
     const createdUrl = `/change-forgoten-password/${password_hash}/${id}`
 
-    // axios.post('http://127.0.0.1:5500/api/send-email', { to: email, subject: "cahnge your password", text: createdUrl }, {
-    //   headers: {
-    //     'Content-Type': 'application/json'
-    //   }
-    // })
-    // .then(response => {
-    //   console.log('Response:', response.data);
-    // })
-    // .catch(error => {
-    //   console.error('Error:', error.message);
-    // });
+    axios.post('http://127.0.0.1:5500/api/send-email', { to: email, subject: "cahnge your password", text: createdUrl }, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log('Response:', response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+    });
 
     return res.status(200).json({message:"email to reset pasword is sent to the email.", paswordChangeUrl:createdUrl})
 }
