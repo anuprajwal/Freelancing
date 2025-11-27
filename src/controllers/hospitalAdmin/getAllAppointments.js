a55a4ac5b19caac3e6ef58a38948eb0188e286e9
ANUPRAJWAL <anuprajwal89@gmail.com>
2025-11-27 14:04:54 +0530

    Revert "ae"
    This reverts commit e736c8f44399734fba6511a00844ac5b0332d060.
    
    reverting...

--- src/controllers/hospitalAdmin/getAllAppointments.js
+++ src/controllers/hospitalAdmin/getAllAppointments.js
@@ -21,7 +21,7 @@ const allAppointments = async (req, res)=>{
                       model: appointments,
                       as: 'appointments',
                       required: true,
-                      attributes: ['id', 'appointment_date', 'appointment_status'], 
+                      attributes: ['id', 'date', 'status'], 
                     }
                   ]
                 }
