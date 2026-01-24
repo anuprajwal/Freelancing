99bc8400b109d51f91f143a38719bad4eb0290a7
ANUPRAJWAL <anuprajwal89@gmail.com>
2026-01-24 09:10:19 +0530

    added new solid program in caller functions

--- src/controllers/caller/states.js
+++ src/controllers/caller/states.js
@@ -1,7 +1,9 @@
-export default CALL_STATUS = {
+const CALL_STATUS = {
     RINGING: "Ringing",
     ANSWERED: "Call Answered",
     IN_PROGRESS: "In Progress",
     REJECTED: "Call Rejected",
     COMPLETED: "Call Completed",
 };
+
+module.exports = CALL_STATUS
