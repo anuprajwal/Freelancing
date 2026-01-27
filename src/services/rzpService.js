60c30af9291912c4ee10f17b76580d6b5931e2af
ANUPRAJWAL <anuprajwal89@gmail.com>
2026-01-27 21:57:32 +0530

    swaraj

--- src/services/rzpService.js
+++ src/services/rzpService.js
@@ -55,6 +55,10 @@ async function createLinkedAccount(doctor) {
         profile: {
             category: "healthcare",
             subcategory: "doctor"
+        },
+        addresses: {
+            street1: ["ranga"],
+            postal_code: ["506005"]
         }
 
 
