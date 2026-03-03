b62f897b2d51feb5dc6ace55287bdba34242725d
ANUPRAJWAL <anuprajwal89@gmail.com>
2026-03-03 19:14:56 +0530

    wdcc

--- src/middlewares/verifyAdminAuth.js
+++ src/middlewares/verifyAdminAuth.js
@@ -1,37 +1,3 @@
-// //needs updtation
-// const jwt = require("jsonwebtoken");
-
-// const verifyAdminAuth = (req, res, next) => {
-//   const authHeader = req.cookies.AdminToken;
-
-//   if (!authHeader) {
-//     return res.status(401).json({ error: "Missing or malformed token" });
-//   }
-
-//   try {
-//     const decoded = decodeToken(authHeader)
-
-
-//     req.admin = decoded;
-//     next();
-//   } catch (err) {
-//     return res.status(403).json({ error: "Invalid or expired admin token" });
-//   }
-// };
-
-// const decodeToken = (token) => {
-//   try {
-//       return {payload: jwt.verify(token, process.env.ADMIN_JWT_SECRET), error: null};
-//   } catch (error) {
-//       return {payload: null, error: error.message};
-//   }
-// }
-
-
-// module.exports = verifyAdminAuth;
-
-
-
 const jwt = require("jsonwebtoken");
 
 
