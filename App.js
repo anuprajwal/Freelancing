1dd29cfb3c4630fe1f9c094454c79e441ef73d4e
anuprajwal <anuprajwal89@gmail.com>
2026-07-24 10:41:17 +0530

    changes token thing

--- App.js
+++ App.js
@@ -91,6 +91,7 @@ const allowedOrigins = [
         "https://auth.docapp.co.in",
         "https://doctors.docapp.co.in",
         "https://users.docapp.co.in",
+        "https://super.docapp.co.in"
 ];
 
 // CORS middleware
--- src/controllers/admin/adminAuthcontroller.js
+++ src/controllers/admin/adminAuthcontroller.js
@@ -1,73 +1,3 @@
-// const {
-//     admin
-// } = require("../../../models");
-// const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-
-// const loginAdmin = async (req, res) => {
-//     const {
-//         email,
-//         password
-//     } = req.body;
-
-//     try {
-//         const existingAdmin = await admin.findOne({
-//             where: {
-//                 email
-//             }
-//         });
-
-//         if (!existingAdmin) {
-//             return res.status(401).json({
-//                 error: "Invalid email or password"
-//             });
-//         }
-
-//         const match = await bcrypt.compare(password, existingAdmin.password);
-
-//         if (!match) {
-//             return res.status(401).json({
-//                 error: "Invalid email or password"
-//             });
-//         }
-
-//         if (!existingAdmin.is_active) {
-//             return res.status(403).json({
-//                 error: "Admin account is inactive"
-//             });
-//         }
-
-//         const token = jwt.sign({
-//                 id: existingAdmin.id,
-//                 email: existingAdmin.email,
-//                 scope: "admin",
-//             },
-//             process.env.ADMIN_JWT_SECRET, {
-//                 expiresIn: "1h"
-//             }
-//         );
-
-//         res.cookie("AdminToken", token, {
-//             httpOnly: true,
-//             secure: true,
-//             sameSite: "None",
-//             domain: ".local.docapp",
-//             maxAge: parseInt("315360000000", 10),
-//         });
-
-//         res.status(200).json({
-//             message: "Login successful"
-//         });
-//     } catch (err) {
-//         res.status(500).json({
-//             error: "Server error"
-//         });
-//     }
-// };
-
-// module.exports = loginAdmin;
-
-
 const {
     admin
 } = require("../../../models");
@@ -142,7 +72,7 @@ const loginAdmin = async (req, res) => {
         // 📱 Mobile / API → Return token in JSON
         return res.status(200).json({
             message: "Login successful",
-            token: clientType !== 'web' ? token : undefined,
+            token: token,
             clientType,
             admin: {
                 id: existingAdmin.id,
