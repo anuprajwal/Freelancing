f723d525a2d5023f017279523dbc89890c3792a9
anuprajwal <anuprajwal89@gmail.com>
2026-08-17 22:21:04 +0530

    created kyc for hospitals

--- src/routes/doctorKycRoutes.js
+++ src/routes/doctorKycRoutes.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const doctorKycController = require("../controllers/payment/doctorKycController");
 const hospitalKycController = require("../controllers/payment/hospitalKycController");
+const protect = require("../middlewares/authMiddleware.js");
 
-router.post("/doctor/:id/start-onboarding", doctorKycController.startOnboarding);
-router.get("/doctor/:id/onboarding-status", doctorKycController.getOnboardingStatus);
-router.post("/hospital/:id/start-onboarding", hospitalKycController.startOnboardingDoctor);
-router.get("/hospital/:id/onboarding-status", hospitalKycController.getOnboardingStatus);
+
+router.post("/doctor/:id/start-onboarding", protect, doctorKycController.startOnboarding);
+router.get("/doctor/:id/onboarding-status", protect, doctorKycController.getOnboardingStatus);
+router.post("/hospital/:id/start-onboarding", protect, hospitalKycController.startOnboardingHospital);
+router.get("/hospital/:id/onboarding-status", protect, hospitalKycController.getOnboardingStatus);
 
 module.exports = router;
