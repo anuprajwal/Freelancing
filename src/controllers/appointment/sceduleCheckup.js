c9e70b2df2a0b19cbb2c46fd2313a4a037560613
anuprajwal <anuprajwal89@gmail.com>
2026-08-19 19:10:12 +0530

    sending full error to the frontend

--- src/controllers/appointment/sceduleCheckup.js
+++ src/controllers/appointment/sceduleCheckup.js
@@ -245,7 +245,7 @@ const scheduleCheckup = async (req, res) => {
   } catch (err) {
     // // logger && logger.error("scheduleCheckup error:", err);
     console.log("scheduleCheckup error:", err);
-    return res.status(500).json({ error: "Internal server error", details: err.message });
+    return res.status(500).json({ error: "Internal server error", details: err.message || err.description || err });
   }
 };
 
