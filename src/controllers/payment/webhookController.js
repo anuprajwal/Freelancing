3b370aaad3b091a9ac4605aef51cf7333a94c734
anuprajwal <anuprajwal89@gmail.com>
2026-08-22 19:31:38 +0530

    working on webhooks

--- src/controllers/payment/webhookController.js
+++ src/controllers/payment/webhookController.js
@@ -19,6 +19,9 @@ exports.handleWebhook = async (req, res) => {
   console.log("Webhook secret:", webhookSecret);
 
   const signature = req.headers["x-razorpay-signature"];
+  const event_check_id = req.headers["x-razorpay-event-id"];
+
+  console.log("Received event ID:", event_check_id);
   const rawBody = req.body;
 
   const expected = crypto
