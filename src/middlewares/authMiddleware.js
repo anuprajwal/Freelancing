a93b5d557bff1ed3b0636d467c2c62abeb2acafb
anuprajwal <anuprajwal89@gmail.com>
2026-09-26 17:02:46 +0530

    added saving config

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -136,10 +136,9 @@ const protect = (req, res, next) => {
         const userRole = decoded.payload.scope; // Adjust based on your decodeToken structure
 
         if (!origin) {
-            // return res.status(403).json({
-            //     error: "Unauthorized access: Missing origin or referer header"
-            // });
-
+            return res.status(403).json({
+                error: "Unauthorized access: Missing origin or referer header"
+            });
         }
 
         // Extract hostname safely from origin/referer (handles http://, https://, ports, and subpaths)
